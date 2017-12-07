bfebb793a449f58d134e50d5d605366577a76745
Philip Varady <pvarady3@outlook.com>
2017-12-07 00:56:32 -0500

    Fixed Global having garbage in it

--- Rosebud/Rosebud/Global.asax.cs
+++ Rosebud/Rosebud/Global.asax.cs
@@ -1,16 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Security;
-using System.Web.SessionState;
-
-namespace Rosebud
-{
-    public class Global : System.Web.HttpApplication
-    {
-        protected void Application_Start(object sender, EventArgs e)
-        {
-        }
-    }
-}
+﻿
